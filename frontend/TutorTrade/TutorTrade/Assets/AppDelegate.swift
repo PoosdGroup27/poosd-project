26a9597d55a4bd656b6672a3301ded48b1252c32
Brock Davis <brockdavis@outlook.com>
2021-09-27 16:04:05 -0400

    Set up Launch Screen

--- frontend/TutorTrade/TutorTrade/Assets/AppDelegate.swift
+++ frontend/TutorTrade/TutorTrade/Assets/AppDelegate.swift
@@ -14,7 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        do {
+            sleep(3)
+        }
+        
         window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 }
