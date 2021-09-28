6d6085ea88b72a6e6669649a9a38bedc005fae2b
Sebastian <32209477+Sebastian1712@users.noreply.github.com>
2021-09-28 08:35:08 -0400

    Fixing merge conflict from PR # 21 (#24)

--- frontend/TutorTrade/TutorTrade/Controllers/Request/RequestController.swift
+++ frontend/TutorTrade/TutorTrade/Controllers/Request/RequestController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class RequestController: UIViewController {
-<<<<<<< HEAD
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -21,26 +20,6 @@ class RequestController: UIViewController {
     }
 
     override func viewDidLoad() {
-    }
-=======
-
-    override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Request"
-        tabBarItem = UITabBarItem(title: "Request", image: UIImage(systemName: "hand.raised"), tag: 1)
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
->>>>>>> main
 }
