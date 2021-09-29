ea21e545ad9013f7ddb6efb1e07abffb7f144f9c
Sebastian <sebaoctavio@gmail.com>
2021-09-29 07:25:43 -0400

    Added SubjectTextField class

---
+++ frontend/TutorTrade/TutorTrade/Controllers/Request/RequestSubjectTextField.swift
@@ -0,0 +1,15 @@
+//
+//  RequestSubjectTextField.swift
+//  TutorTrade
+//
+//  Created by Sebastian Hernandez on 9/29/21.
+//
+
+import UIKit
+
+class RequestSubjectTextField: UITextField {
+    
+    convenience init() {
+        self.init(frame: CGRect.zero)
+    }
+}
