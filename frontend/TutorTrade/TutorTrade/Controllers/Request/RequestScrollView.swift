0c5050a1a68b4d18d8d69f0dcdcbea754340c4b7
Sebastian <sebaoctavio@gmail.com>
2021-09-30 20:20:46 -0400

    added didTap in scrollview

--- frontend/TutorTrade/TutorTrade/Controllers/Request/RequestScrollView.swift
+++ frontend/TutorTrade/TutorTrade/Controllers/Request/RequestScrollView.swift
@@ -16,11 +16,24 @@ class RequestScrollView: UIScrollView {
         let textField = SubjectRequestView()
         return textField
     }()
+    
+    let submitRequestButton: UIButton! = {
+        let button = SubmitRequestButton()
+        return button
+    }()
 
     convenience init(scrollWidth: CGFloat, scrollHeight: CGFloat) {
         self.init(frame: CGRect(x: 10, y: 10, width: scrollWidth, height: scrollHeight))
         self.backgroundColor = .white
         self.contentSize = CGSize(width: 0, height: scrollHeight * 2)
         self.addSubview(subjectRequestView)
+        self.addSubview(submitRequestButton)
+        
+        submitRequestButton.addTarget(self, action: #selector(didTapSubmitRequestButton), for: .touchUpInside)
+    }
+    
+    @objc func didTapSubmitRequestButton() {
+        print("Pressed the button!")
+        print(subjectRequestView.text ?? "no text entered")
     }
 }
---
+++ frontend/TutorTrade/TutorTrade/Controllers/Request/SubmitRequestButton.swift
@@ -0,0 +1,21 @@
+//
+//  SubmitRequestButton.swift
+//  TutorTrade
+//
+//  Created by Sebastian Hernandez on 9/29/21.
+//
+
+import UIKit
+
+class SubmitRequestButton: UIButton {
+    convenience init() {
+        self.init(frame: CGRect(x: 250, y: 1000, width: 100, height: 50))
+        self.backgroundColor = .blue
+        self.setTitle("Submit", for: .normal)
+//        self.addTarget(self, action: #selector(didTapSubmitRequestButton), for: .touchUpInside)
+    }
+    
+//    @objc func didTapSubmitRequestButton() {
+//        print("Pressed the button!")
+//    }
+}
