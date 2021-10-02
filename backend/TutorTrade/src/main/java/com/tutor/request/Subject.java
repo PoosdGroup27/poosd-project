ac0d2260b87d2e511074d848e3e3555b70bbcdcb
Adam Fernandes <fernadam8@gmail.com>
2021-10-02 15:01:48 -0400

    edited subjects enum

--- backend/TutorTrade/src/main/java/com/tutor/request/Subject.java
+++ backend/TutorTrade/src/main/java/com/tutor/request/Subject.java
@@ -1,6 +1,13 @@
 package com.tutor.request;
 
-
+/**
+ * Enum of different subjects users can be tutored for.
+ */
 public enum Subject {
-    BIO, CHEM, PHY, CS, LIT, UNSUPPORTED
+    BIO,
+    CHEM,
+    PHY,
+    CS,
+    LIT,
+    UNSUPPORTED
 }
