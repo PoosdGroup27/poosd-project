22a62198f3d4faff0213f3817721f95bd528974b
Adam Fernandes <fernadam8@gmail.com>
2021-10-05 18:02:08 -0400

    removed newline between imports

--- backend/TutorTrade/src/main/java/com/tutor/request/RequestsHandler.java
+++ backend/TutorTrade/src/main/java/com/tutor/request/RequestsHandler.java
@@ -15,7 +15,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
