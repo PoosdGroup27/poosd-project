9925985f2e73a07d2f4c5255afba412af8532d9e
Sebastian <sebaoctavio@gmail.com>
2021-10-05 08:38:00 -0400

    started styling points component

--- frontend/TutorTrade/TutorTrade/Controllers/Request/PointsView.swift
+++ frontend/TutorTrade/TutorTrade/Controllers/Request/PointsView.swift
@@ -8,28 +8,48 @@ import UIKit
 
 class PointsView: UIView {
 
-    // we need to format the view
+    // we need to format the view ✅
     // create the label outside of view - ✅
-    // create the textfield
     // create the label inside of view
     // create the buttons
     
     let pointsLabel: UILabel! = {
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 374, height: 50))
+        let label = UILabel(frame: CGRect(x: 0, y: -50, width: 374, height: 50))
         
         guard let pointsLabel = UIFont(name: "Lato-Bold", size: 25) else {
             fatalError("Failed to load Lato-Font")
         }
         
         label.font = UIFontMetrics.default.scaledFont(for: pointsLabel)
-        label.text = "Cost"
+        label.text = "Budget"
 
         return label
     }()
     
+    // create the textfield
+    let pointsTextField: UITextField! = {
+        let textField = UITextField(frame: CGRect(x: 15, y: 15, width: 110, height: 50))
+        
+        guard let pointsTextFieldFont = UIFont(name: "Lato-Bold", size: 20) else {
+            fatalError("Failed to load Lato-Font")
+        }
+        
+        textField.backgroundColor = .white
+        textField.textAlignment = .center
+        textField.textColor = .black
+        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 20
+        textField.placeholder = "0"
+        textField.font = UIFontMetrics.default.scaledFont(for: pointsTextFieldFont)
+        
+        return textField
+    }()
+    
     convenience init() {
-        self.init(frame: CGRect(x: 0, y: 620, width: 350, height: 150))
-        self.backgroundColor = .red
+        self.init(frame: CGRect(x: 0, y: 660, width: 350, height: 150))
+        self.backgroundColor = .systemGreen
+        self.layer.cornerRadius = 20
         self.addSubview(pointsLabel)
+        self.addSubview(pointsTextField)
     }
 }
