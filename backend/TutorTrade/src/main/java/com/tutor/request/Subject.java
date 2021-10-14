b3a05ac7a6860c00811f0a7b0b8f2979d3166035
Adam Fernandes <fernadam8@gmail.com>
2021-10-13 22:25:44 -0400

    subjects enum has emoji and new helpers

--- backend/TutorTrade/src/main/java/com/tutor/request/Subject.java
+++ backend/TutorTrade/src/main/java/com/tutor/request/Subject.java
@@ -1,13 +1,35 @@
 package com.tutor.request;
 
+import java.util.Arrays;
+
 /**
- * Enum of different subjects users can be tutored for.
+ * Enum of different subjects users can be tutored for. Format of underlying string is
+ * <name>:<emoji>
  */
 public enum Subject {
-    BIO,
-    CHEM,
-    PHY,
-    CS,
-    LIT,
-    UNSUPPORTED
+  BIO("BIOLOGY:🧬"),
+  CHEM("CHEMISTRY:🧪"),
+  PHY("PHYSICS:🚗"),
+  CS("COMPUTER SCIENCE:💻"),
+  LIT("LITERATURE:📚"),
+  UNSUPPORTED("UNSUPPORTED:🤷");
+
+  private final String subjectName;
+
+  Subject(String subjectName) {
+    this.subjectName = subjectName;
+  }
+
+  @Override
+  public String toString() {
+    return this.subjectName;
+  }
+
+  public String getEmoji() {
+    return toString().split(":")[1];
+  }
+
+  public String getSubjectName() {
+    return toString().split(":")[0];
+  }
 }
