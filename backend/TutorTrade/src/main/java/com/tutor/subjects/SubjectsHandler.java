66c7471d9a62a042b5f4280daae78c9a7164bc6a
Jesse Gingold <70174673+JesseG354@users.noreply.github.com>
2021-10-14 21:52:05 -0400

    Modify subject handler to return list of subjects (#59)
    * Modify subject handler to return list of subjects and allow lambda to serialize, rather than serialize in handler.
    
    * Remove now unnecessary exception handling.

--- backend/TutorTrade/src/main/java/com/tutor/subjects/SubjectsHandler.java
+++ backend/TutorTrade/src/main/java/com/tutor/subjects/SubjectsHandler.java
@@ -2,42 +2,25 @@ package com.tutor.subjects;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.tutor.request.Subject;
-import com.tutor.utils.ApiUtils;
-import java.net.HttpURLConnection;
-import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /** Handle requests to the Subjects endpoint. Currently only supports get requests. */
-public class SubjectsHandler implements RequestHandler<Map<Object, Object>, String> {
-
+public class SubjectsHandler implements RequestHandler<Map<Object, Object>, List<String>> {
   private static final Logger LOG = LogManager.getLogger(SubjectsHandler.class);
 
   @Override
-  public String handleRequest(Map<Object, Object> event, Context context) {
+  public List<String> handleRequest(Map<Object, Object> event, Context context) {
     HashMap<?, ?> contextMap = (HashMap<?, ?>) event.get("context");
     String httpMethod = (String) contextMap.get("http-method");
 
     if (httpMethod.equals("GET")) {
-      try {
-        return getListOfSubjects();
-      } catch (JsonProcessingException e) {
-        return ApiUtils.returnErrorString(e);
-      }
+      return Subject.getListOfSubjects();
     }
-
-    return ApiUtils.getResponseAsString(
-        HttpURLConnection.HTTP_BAD_METHOD, "Requested method was not found.");
-  }
-
-  /**
-   * Returns a list of available subjects of the Subjects enum
-   *
-   * @return JSONified string with all available subjects
-   */
-  private String getListOfSubjects() throws JsonProcessingException {
-    return ApiUtils.getResponseAsString(HttpURLConnection.HTTP_OK, Subject.getListOfSubjectsAsString());
+    return null;
   }
 }
