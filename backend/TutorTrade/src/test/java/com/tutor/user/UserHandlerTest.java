03fa1e551052f4d646d1febecf0e3d97a295f7c5
Adam Fernandes <fernadam8@gmail.com>
2021-10-16 18:33:14 -0400

    removed unused imports in userhandler class

--- backend/TutorTrade/src/test/java/com/tutor/user/UserHandlerTest.java
+++ backend/TutorTrade/src/test/java/com/tutor/user/UserHandlerTest.java
@@ -5,13 +5,9 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tutor.request.Request;
-import com.tutor.utils.ApiResponse;
 import com.tutor.utils.ApiUtils;
-import com.tutor.utils.RequestUtils;
 import com.tutor.utils.UserUtils;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
