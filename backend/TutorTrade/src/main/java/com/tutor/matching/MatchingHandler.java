84de43af0b9f0b5ee9265dc04327eb5ae0d16d47
Jesse Gingold <jgingold@knights.ucf.edu>
2021-11-18 14:17:25 -0500

    Updating matching to create database entry for new users.

--- backend/TutorTrade/src/main/java/com/tutor/matching/MatchingHandler.java
+++ backend/TutorTrade/src/main/java/com/tutor/matching/MatchingHandler.java
@@ -22,6 +22,8 @@ import com.tutor.request.Status;
 import com.tutor.utils.ApiResponse;
 import com.tutor.utils.ApiUtils;
 import com.tutor.utils.RequestUtils;
+import lombok.SneakyThrows;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.util.*;
@@ -31,41 +33,19 @@ import java.util.*;
  * who are well suited to fulfill that request.
  */
 public class MatchingHandler implements RequestHandler<DynamodbEvent, ApiResponse<?>> {
-  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-  private static final String STAGE =
-      System.getenv("STAGE").replace('-', '_').toUpperCase(Locale.ENGLISH);
-  private static final String KNN_BUCKET =
-      String.format("request-normalized-data-%s", System.getenv("STAGE"));
+
   private static final AmazonDynamoDB DYNAMO_DB =
       AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
   private static final DynamoDBMapper DYNAMO_DB_MAPPER = new DynamoDBMapper(DYNAMO_DB);
-  private static final AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
-  private static final String KNN_MODEL = MatchingConstants.KNN_MODEL;
 
+
+  @SneakyThrows
   @Override
   public ApiResponse<?> handleRequest(DynamodbEvent dynamodbEvent, Context context) {
-    // get normalized, serialized request data from S3
-    List<RequestKnnData> data;
-    if (!s3Client.doesObjectExist(KNN_BUCKET, KNN_MODEL)) {
-      data = new ArrayList<>();
-    } else {
-      S3Object object = s3Client.getObject(KNN_BUCKET, KNN_MODEL);
-      S3ObjectInputStream input = object.getObjectContent();
-      try {
-        CollectionType requestArray =
-            OBJECT_MAPPER
-                .getTypeFactory()
-                .constructCollectionType(List.class, RequestKnnData.class);
-        data = OBJECT_MAPPER.readValue(input, requestArray);
-      } catch (IOException e) {
-        return ApiUtils.returnErrorResponse(e);
-      }
-    }
-
     // sanity check to make sure we're not stuck in a recursive record processing loop, since
     // we modify the same dynamodb table here as is processed by this lambda.
-    // We will never create more than 15 matches during testing
-    assert dynamodbEvent.getRecords().size() < 15;
+    // We will never create more than 5 matches during testing
+    assert dynamodbEvent.getRecords().size() < 5;
 
     for (DynamodbEvent.DynamodbStreamRecord record : dynamodbEvent.getRecords()) {
       Map<String, AttributeValue> oldRequestRecord = record.getDynamodb().getOldImage();
@@ -80,12 +60,16 @@ public class MatchingHandler implements RequestHandler<DynamodbEvent, ApiRespons
             .build();
       }
 
+      // request needs to be matched
       if (newRequestRecord.get("status").getS().equals("PENDING")) {
+        List<RequestKnnData> data = MatchingUtils.getRequestDate();
         getMatches(data, newRequest);
         System.out.println("Matching completed.");
       } else if (!oldRequestRecord.isEmpty()
           && (!oldRequestRecord.get("status").getS().equals("COMPLETED")
               && newRequestRecord.get("status").getS().equals("COMPLETED"))) {
+        // request is now completed, add to data store
+        MatchingUtils.updateRequestDataStore(newRequest);
         System.out.println("Update completed.");
       } else {
         System.out.println("Returning without action.");
@@ -97,7 +81,6 @@ public class MatchingHandler implements RequestHandler<DynamodbEvent, ApiRespons
             .build();
       }
     }
-
     return ApiResponse.<String>builder()
         .statusCode(HttpURLConnection.HTTP_OK)
         .body("Matching successful")
@@ -133,27 +116,6 @@ public class MatchingHandler implements RequestHandler<DynamodbEvent, ApiRespons
             .build());
   }
 
-  /**
-   * Update our s3 request data store with request information once a request has been completed.
-   * TODO: This method has side effects and modifying the dynamo table will trigger a new stream.
-   * This works for now but is not ideal.
-   *
-   * @param data All our existing request data. In the future we may need to check that this is
-   *     small enough to bring into memory.
-   * @param request The request we're adding to the data store.
-   * @return [requestId]
-   */
-  private List<UUID> updateRequestDataStore(List<RequestKnnData> data, Request request) {
-    data.add(new RequestKnnData(request));
-    try {
-      String jsonArray = OBJECT_MAPPER.writeValueAsString(data);
-      s3Client.putObject(KNN_BUCKET, KNN_MODEL, jsonArray);
-    } catch (JsonProcessingException e) {
-      e.printStackTrace();
-      return null;
-    }
-    List<UUID> result = new ArrayList<>();
-    result.add(request.getRequestId());
-    return result;
-  }
+
+
 }
