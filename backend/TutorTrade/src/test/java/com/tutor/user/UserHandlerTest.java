3747d45aca9ac06cedfa54bd6be77533a9145800
Adam Fernandes <fernadam8@gmail.com>
2021-11-20 15:12:07 -0500

    post asserting equals skeleton

--- backend/TutorTrade/src/test/java/com/tutor/user/UserHandlerTest.java
+++ backend/TutorTrade/src/test/java/com/tutor/user/UserHandlerTest.java
@@ -5,17 +5,23 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tutor.request.Request;
 import com.tutor.utils.ApiUtils;
 import com.tutor.utils.JsonUtils;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import com.tutor.utils.RequestUtils;
+import com.tutor.utils.UserUtils;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class UserHandlerTest {
     private static final AmazonDynamoDB DYNAMO_DB =
@@ -31,24 +37,43 @@ public class UserHandlerTest {
     ArrayList<String> createdUsers = new ArrayList<>();
     private static final String TEST_USER_ID = UUID.randomUUID().toString();
 
-    @BeforeAll
-    static void beforeAll() throws IOException {
+    private static Map<String, String> getTestUserFromFile(String userBody) throws IOException {
+        return OBJECT_MAPPER.readValue(userBody, new TypeReference<HashMap<String, String>>() {});
+    }
+
+    @Test
+    void postUserGivenValidFields() throws IOException {
+        // GIVEN: valid user fields
         String userBody = JSON_UTILS.getJsonFromFileAsString("validUserToPost.json");
-        ApiUtils.put(ApiUtils.ApiStages.TEST.toString(), "/user/" + TEST_USER_ID, userBody);
-    }
+        Map<String, String> userBodyFields = getTestUserFromFile(userBody);
 
-    @AfterAll
-    static void afterAll() {
-        ApiUtils.delete(ApiUtils.ApiStages.TEST.toString(), "user/" + TEST_USER_ID);
-    }
+        // WHEN: post user to user API
+        String userPostResponseString = ApiUtils.post(ApiUtils.ApiStages.TEST.toString(), "/user", OBJECT_MAPPER.writeValueAsString(userBodyFields));
 
-    @BeforeEach
-    void setUp() {}
+        // THEN: request response is not null
+        assertNotNull(userPostResponseString);
 
-    @AfterEach
-    void tearDown() {
-        for (String requestId : createdUsers) {
-            ApiUtils.delete(ApiUtils.ApiStages.TEST.toString(), String.format("/user/%s", requestId));
-        }
+        // THEN: user response is a valid user object
+        User user = UserUtils.getUserFromAPIResponse(userPostResponseString);
+        assertNotNull(user);
+
+        // THEN: fields of request object match those from validPostRequest.json
+
+        assertEquals(user.getSchool(), userBodyFields.get("school"));
+        assertEquals(user.getName(), userBodyFields.get("name"));
+
+        assertEquals(user.getIsActive(), Boolean.valueOf(userBodyFields.get("isActive")));
+        assertEquals(user.getPoints(), Integer.parseInt(userBodyFields.get("points")));
+        assertEquals(user.getSessionIds(), userBodyFields.get("sessionIds"));
+        assertEquals(user.getUserId(), userBodyFields.get("userId"));
+        assertEquals(user.getPhoneNumber(), userBodyFields.get("phoneNumber"));
+        assertEquals(user.getSubjectsLearn(), userBodyFields.get("subjectsLearn"));
+        assertEquals(user.getSubjectsTeach(), userBodyFields.get("subjectsTeach"));
+        assertEquals(user.getCumulativeSessionsCompleted(), userBodyFields.get("cumulativeSessionsCompleted"));
+        assertEquals(user.getRating(), userBodyFields.get("rating"));
+        assertEquals(user.getMajor(), userBodyFields.get("major"));
+        assertEquals(user.getReviewEvaluations(), userBodyFields.get("reviewEvaluations"));
+
+
     }
 }
