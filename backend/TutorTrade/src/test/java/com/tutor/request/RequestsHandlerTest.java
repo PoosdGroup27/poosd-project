6422a60a91f1326634e0fef82911a7bfaf249a1e
Jesse Gingold <70174673+JesseG354@users.noreply.github.com>
2021-11-20 15:45:03 -0500

    Add in random tests for request (#132)
    * Automatically create a chat between users with phone number when a user "accepts" request
    
    * Change stages to prod
    
    * Fix typo in error message where user does not exist
    
    * Create random requests for testing purposes
    
    * fixed faults
    
    * Add test case numbers corresponding to deliverable 2
    
    Co-authored-by: Adam Fernandes <fernadam8@gmail.com>

--- backend/TutorTrade/src/test/java/com/tutor/request/RequestsHandlerTest.java
+++ backend/TutorTrade/src/test/java/com/tutor/request/RequestsHandlerTest.java
@@ -8,6 +8,7 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tutor.subject.Subject;
 import com.tutor.utils.ApiUtils;
 import com.tutor.utils.JsonUtils;
 import com.tutor.utils.RequestUtils;
@@ -64,6 +65,7 @@ class RequestsHandlerTest {
     return requestBodyFields;
   }
 
+  // Test-Case-B01.2
   @Test
   void postTestGivenValidRequest() throws IOException {
     // GIVEN: valid request body
@@ -85,7 +87,7 @@ class RequestsHandlerTest {
     assertNotNull(request);
 
     // THEN: fields of request object match those from validPostRequest.json
-    assertEquals(request.getRequesterId().toString(), requestBodyFields.get("requesterId"));
+    assertEquals(request.getRequesterId(), requestBodyFields.get("requesterId"));
     assertEquals(request.getSubject().getSubjectName(), requestBodyFields.get("subject"));
     assertEquals(request.getCostInPoints().toString(), requestBodyFields.get("costInPoints"));
     assertEquals(request.getUrgency().toString(), requestBodyFields.get("urgency"));
@@ -102,7 +104,7 @@ class RequestsHandlerTest {
     Request requestInDB = requestById.get(0);
 
     // THEN: all fields correct
-    assertEquals(requestInDB.getRequesterId().toString(), requestBodyFields.get("requesterId"));
+    assertEquals(requestInDB.getRequesterId(), requestBodyFields.get("requesterId"));
     assertEquals(requestInDB.getSubject().getSubjectName(), requestBodyFields.get("subject"));
     assertEquals(requestInDB.getCostInPoints().toString(), requestBodyFields.get("costInPoints"));
     assertEquals(requestInDB.getUrgency().toString(), requestBodyFields.get("urgency"));
@@ -112,6 +114,55 @@ class RequestsHandlerTest {
     createdRequests.add(request.getRequestId().toString());
   }
 
+  // Test-Case-B01.1
+  @Test
+  void postTestGivenValidRandomRequest() throws IOException {
+    // GIVEN: valid random request body
+    Map<String, String> requestBodyFields = createRandomRequestPostBody();
+
+    // WHEN: post request to request API
+    String requestPostResponseString =
+            ApiUtils.post(
+                    ApiUtils.ApiStages.TEST.toString(),
+                    "/request",
+                    OBJECT_MAPPER.writeValueAsString(requestBodyFields));
+
+    // THEN: request response is not null
+    assertNotNull(requestPostResponseString);
+
+    // THEN: request response is a valid request object
+    Request request = RequestUtils.getRequestFromAPIResponse(requestPostResponseString);
+    assertNotNull(request);
+
+    // THEN: fields of request object match those from validPostRequest.json
+    assertEquals(request.getRequesterId(), requestBodyFields.get("requesterId"));
+    assertEquals(request.getSubject().getSubjectName(), requestBodyFields.get("subject"));
+    assertEquals(request.getCostInPoints().toString(), requestBodyFields.get("costInPoints"));
+    assertEquals(request.getUrgency().toString(), requestBodyFields.get("urgency"));
+    assertEquals(request.getPlatform().toString(), requestBodyFields.get("platform"));
+
+    // WHEN: query dynamodb directly so as not to rely on correctness of GET method
+    Request key = new Request(request.getRequestId());
+    DynamoDBQueryExpression<Request> queryExpression =
+            new DynamoDBQueryExpression<Request>().withHashKeyValues(key);
+
+    List<Request> requestById =
+            DYNAMO_DB_MAPPER.query(Request.class, queryExpression, DYNAMO_DB_MAPPER_CONFIG);
+    assertEquals(1, requestById.size());
+    Request requestInDB = requestById.get(0);
+
+    // THEN: all fields correct
+    assertEquals(requestInDB.getRequesterId(), requestBodyFields.get("requesterId"));
+    assertEquals(requestInDB.getSubject().getSubjectName(), requestBodyFields.get("subject"));
+    assertEquals(requestInDB.getCostInPoints().toString(), requestBodyFields.get("costInPoints"));
+    assertEquals(requestInDB.getUrgency().toString(), requestBodyFields.get("urgency"));
+    assertEquals(requestInDB.getPlatform().toString(), requestBodyFields.get("platform"));
+
+    // cleanup
+    createdRequests.add(request.getRequestId().toString());
+  }
+
+  // Test-Case-B02.2
   @Test
   void patchTestGivenValidChange() throws IOException, RequestBuilderException {
     // GIVEN: valid request body
@@ -141,7 +192,7 @@ class RequestsHandlerTest {
     Request requestInDB = requestById.get(0);
 
     // THEN: all fields correct
-    assertEquals(requestInDB.getRequesterId().toString(), requestBodyFields.get("requesterId"));
+    assertEquals(requestInDB.getRequesterId(), requestBodyFields.get("requesterId"));
     assertEquals(requestInDB.getSubject().getSubjectName(), requestBodyFields.get("subject"));
     assertEquals(requestInDB.getCostInPoints().toString(), requestBodyFields.get("costInPoints"));
     assertEquals(requestInDB.getUrgency().toString(), requestBodyFields.get("urgency"));
@@ -171,7 +222,7 @@ class RequestsHandlerTest {
     assertEquals(1, requestById2.size());
     Request requestInDB2 = requestById2.get(0);
 
-    assertEquals(requestInDB2.getRequesterId().toString(), requestBodyFields.get("requesterId"));
+    assertEquals(requestInDB2.getRequesterId(), requestBodyFields.get("requesterId"));
     assertEquals(requestInDB2.getSubject().getSubjectName(), changes.get("subject"));
     assertEquals(requestInDB2.getCostInPoints().toString(), changes.get("costInPoints"));
     assertEquals(requestInDB2.getUrgency().toString(), requestBodyFields.get("urgency"));
@@ -181,6 +232,80 @@ class RequestsHandlerTest {
     createdRequests.add(request.getRequestId().toString());
   }
 
+
+  // Test-Case-B02.1
+  @Test
+  void patchTestGivenValidRandomChange() throws IOException, RequestBuilderException {
+    Random random = new Random();
+
+    // GIVEN: valid request body
+    String requestBody = JSON_UTILS.getJsonFromFileAsString("validPostRequest.json");
+    Map<String, String> requestBodyFields = getTestRequestFromFile(requestBody);
+
+    // GIVEN: valid request in DynamoDB. Put directly so as not to rely on POST method correctness
+    Request request =
+            new RequestBuilder()
+                    .withRequesterId(requestBodyFields.get("requesterId"))
+                    .withSubject(requestBodyFields.get("subject"))
+                    .withCost(Integer.parseInt(requestBodyFields.get("costInPoints")))
+                    .withUrgency(requestBodyFields.get("urgency"))
+                    .withPlatform(requestBodyFields.get("platform"))
+                    .withDescription(requestBodyFields.get("description"))
+                    .withStatus(Status.PENDING.toString())
+                    .build();
+    DYNAMO_DB_MAPPER.save(request, DYNAMO_DB_MAPPER_CONFIG);
+
+    // WHEN: query dynamodb directly so as not to rely on correctness of GET method
+    Request key = new Request(request.getRequestId());
+    DynamoDBQueryExpression<Request> queryExpression =
+            new DynamoDBQueryExpression<Request>().withHashKeyValues(key);
+    List<Request> requestById =
+            DYNAMO_DB_MAPPER.query(Request.class, queryExpression, DYNAMO_DB_MAPPER_CONFIG);
+    assertEquals(1, requestById.size());
+    Request requestInDB = requestById.get(0);
+
+    // THEN: all fields correct
+    assertEquals(requestInDB.getRequesterId(), requestBodyFields.get("requesterId"));
+    assertEquals(requestInDB.getSubject().getSubjectName(), requestBodyFields.get("subject"));
+    assertEquals(requestInDB.getCostInPoints().toString(), requestBodyFields.get("costInPoints"));
+    assertEquals(requestInDB.getUrgency().toString(), requestBodyFields.get("urgency"));
+    assertEquals(requestInDB.getPlatform().toString(), requestBodyFields.get("platform"));
+
+    // WHEN: patch request
+    Map<String, String> changes = new HashMap<>();
+    changes.put("subject", Subject.values()[random.nextInt(Subject.values().length)].getSubjectName());
+    changes.put("costInPoints", String.valueOf(random.nextInt()));
+    String body = OBJECT_MAPPER.writeValueAsString(changes);
+    String response =
+            ApiUtils.patch(
+                    ApiUtils.ApiStages.TEST.toString(),
+                    String.format("/request/%s", request.getRequestId()),
+                    body);
+
+    // THEN: response is not null
+    assertNotNull(response);
+
+    // THEN: fields in DynamoDB reflect changes. Query directly so as not to rely on correctness of
+    // GET method
+    Request key2 = new Request(request.getRequestId());
+    DynamoDBQueryExpression<Request> queryExpression2 =
+            new DynamoDBQueryExpression<Request>().withHashKeyValues(key);
+    List<Request> requestById2 =
+            DYNAMO_DB_MAPPER.query(Request.class, queryExpression2, DYNAMO_DB_MAPPER_CONFIG);
+    assertEquals(1, requestById2.size());
+    Request requestInDB2 = requestById2.get(0);
+
+    assertEquals(requestInDB2.getRequesterId(), requestBodyFields.get("requesterId"));
+    assertEquals(requestInDB2.getSubject().getSubjectName(), changes.get("subject"));
+    assertEquals(requestInDB2.getCostInPoints().toString(), changes.get("costInPoints"));
+    assertEquals(requestInDB2.getUrgency().toString(), requestBodyFields.get("urgency"));
+    assertEquals(requestInDB2.getPlatform().toString(), requestBodyFields.get("platform"));
+
+    // cleanup
+    createdRequests.add(request.getRequestId().toString());
+  }
+
+  // Test-Case-B04.2
   @Test
   void deleteTestGivenValidRequest() throws IOException, RequestBuilderException {
     // GIVEN: valid request body
@@ -219,6 +344,45 @@ class RequestsHandlerTest {
     assertEquals(0, requestById.size());
   }
 
+  // Test-Case-B04.1
+  @Test
+  void deleteTestGivenValidRandomRequest() throws RequestBuilderException {
+    // GIVEN: valid request body
+    Map<String, String> requestBodyFields = createRandomRequestPostBody();
+
+    // GIVEN: valid request in DynamoDB. Put directly so as not to rely on POST method correctness
+    Request request =
+            new RequestBuilder()
+                    .withRequesterId(requestBodyFields.get("requesterId"))
+                    .withSubject(requestBodyFields.get("subject"))
+                    .withCost(Integer.parseInt(requestBodyFields.get("costInPoints")))
+                    .withUrgency(requestBodyFields.get("urgency"))
+                    .withPlatform(requestBodyFields.get("platform"))
+                    .withDescription(requestBodyFields.get("description"))
+                    .withStatus(Status.PENDING.toString())
+                    .build();
+    DYNAMO_DB_MAPPER.save(request, DYNAMO_DB_MAPPER_CONFIG);
+
+    // WHEN: delete request
+    String response =
+            ApiUtils.delete(
+                    ApiUtils.ApiStages.TEST.toString(),
+                    String.format("/request/%s", request.getRequestId()));
+
+    // THEN: response is not null
+    assertNotNull(response);
+
+    // THEN: delete removes request from DB. Query directly so as not to rely on correctness of GET
+    // method
+    Request key = new Request(request.getRequestId());
+    DynamoDBQueryExpression<Request> queryExpression =
+            new DynamoDBQueryExpression<Request>().withHashKeyValues(key);
+    List<Request> requestById =
+            DYNAMO_DB_MAPPER.query(Request.class, queryExpression, DYNAMO_DB_MAPPER_CONFIG);
+    assertEquals(0, requestById.size());
+  }
+
+  // Test-Case-B03.2
   @Test
   void getTestGivenValidRequest() throws IOException, RequestBuilderException {
     // GIVEN: valid request body
@@ -250,5 +414,52 @@ class RequestsHandlerTest {
     // THEN: object we receive is identical to object we put
     Request requestFromGet = RequestUtils.getRequestFromAPIResponse(response);
     assertEquals(request, requestFromGet);
+  }
+
+  // Test-Case-B03.1
+  @Test
+  void getTestGivenValidRandomRequest() throws IOException, RequestBuilderException {
+    // GIVEN: valid request body
+    Map<String, String> requestBodyFields = createRandomRequestPostBody();
+
+    // GIVEN: valid request in DynamoDB. Put directly so as not to rely on POST method correctness
+    Request request =
+            new RequestBuilder()
+                    .withRequesterId(requestBodyFields.get("requesterId"))
+                    .withSubject(requestBodyFields.get("subject"))
+                    .withCost(Integer.parseInt(requestBodyFields.get("costInPoints")))
+                    .withUrgency(requestBodyFields.get("urgency"))
+                    .withPlatform(requestBodyFields.get("platform"))
+                    .withDescription(requestBodyFields.get("description"))
+                    .withStatus(Status.PENDING.toString())
+                    .build();
+    DYNAMO_DB_MAPPER.save(request, DYNAMO_DB_MAPPER_CONFIG);
+
+    // WHEN: get request
+    String response =
+            ApiUtils.get(
+                    ApiUtils.ApiStages.TEST.toString(),
+                    String.format("/request/%s", request.getRequestId()));
+
+    // THEN: response is not null
+    assertNotNull(response);
+
+    // THEN: object we receive is identical to object we put
+    Request requestFromGet = RequestUtils.getRequestFromAPIResponse(response);
+    assertEquals(request, requestFromGet);
+  }
+
+  private Map<String, String> createRandomRequestPostBody() {
+    Random random = new Random();
+
+    Map<String, String> requestBodyFields = new HashMap<>();
+    requestBodyFields.put("requesterId", TEST_USER_ID);
+    requestBodyFields.put("subject", Subject.values()[random.nextInt(Subject.values().length)].getSubjectName());
+    requestBodyFields.put("urgency", Urgency.values()[random.nextInt(Urgency.values().length)].toString());
+    requestBodyFields.put("platform", Platform.values()[random.nextInt(Platform.values().length)].toString());
+    requestBodyFields.put("costInPoints", String.valueOf(random.nextInt(10000)));
+    requestBodyFields.put("description", "This is a test");
+
+    return requestBodyFields;
   }
 }
