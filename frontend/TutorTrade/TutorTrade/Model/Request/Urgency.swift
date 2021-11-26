2190301294f7ff3f13a7175c53a4a5ab702f2ad5
Sebastian <sebaoctavio@gmail.com>
2021-11-25 21:09:59 -0500

    Refactored request page to fit the style of the rest of application.

---
+++ frontend/TutorTrade/TutorTrade/Model/Request/Urgency.swift
@@ -0,0 +1,16 @@
+//
+//  Urgency.swift
+//  TutorTrade
+//
+//  Created by Sebastian Hernandez on 11/23/21.
+//
+
+import Foundation
+
+import Foundation
+
+enum Urgency: String {
+    case Today = "TODAY"
+    case Tomorrow = "TOMORROW"
+    case ThisWeek = "THIS_WEEK"
+}
