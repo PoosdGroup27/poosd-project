c39dbdc232d6ff637e16f906c80d8ac48c805fab
Jesse Gingold <70174673+JesseG354@users.noreply.github.com>
2021-11-29 20:15:12 -0500

    Jesse/transfer points (#151)
    * Transfer points when session is completed
    
    * Set helperId on accepted
    
    * Move transfer points logic to status update handler

--- backend/TutorTrade/src/main/java/com/tutor/matches/MatchesHandler.java
+++ backend/TutorTrade/src/main/java/com/tutor/matches/MatchesHandler.java
@@ -99,12 +99,6 @@ public class MatchesHandler implements RequestStreamHandler {
 
     requestToUpdate.getOrderedMatches().put(tutorId, matchStatus);
 
-    DYNAMO_DB_MAPPER.save(
-        requestToUpdate,
-        DynamoDBMapperConfig.builder()
-            .withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.UPDATE_SKIP_NULL_ATTRIBUTES)
-            .build());
-
     // if the status update is going to chat, create a new chat between the tutor
     // and tutee
     if (statusUpdate.equals("CHATTING")) {
@@ -138,9 +132,52 @@ public class MatchesHandler implements RequestStreamHandler {
               .build());
     }
 
+    if (statusUpdate.equals("ACCEPTED")) {
+      requestToUpdate.setHelperId(tutorId);
+    }
+
+    DYNAMO_DB_MAPPER.save(
+            requestToUpdate,
+            DynamoDBMapperConfig.builder()
+                    .withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.UPDATE_SKIP_NULL_ATTRIBUTES)
+                    .build());
+
+    if (statusUpdate.equals("COMPLETED")) {
+      transferPoints(tutorId, requestToUpdate.getRequesterId(), requestToUpdate.getCostInPoints());
+    }
+
     return ApiResponse.<Request>builder()
         .statusCode(HttpURLConnection.HTTP_OK)
         .body(requestToUpdate)
         .build();
+  }
+
+  private void transferPoints(String tutorId, String tuteeId, int cost) {
+    User tutor = UserUtils.getUserObjectById(tutorId);
+
+    // this should never happen, given valid requests
+    if (tutor == null) {
+      return;
+    }
+    tutor.setPoints(tutor.getPoints() + cost);
+
+    DYNAMO_DB_MAPPER.save(
+            tutor,
+            DynamoDBMapperConfig.builder()
+                    .withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.UPDATE_SKIP_NULL_ATTRIBUTES)
+                    .build());
+
+    User tutee = UserUtils.getUserObjectById(tuteeId);
+    // this should never happen, given valid requests
+    if (tutee == null) {
+      return;
+    }
+    tutee.setPoints(tutee.getPoints() - cost);
+
+    DYNAMO_DB_MAPPER.save(
+            tutee,
+            DynamoDBMapperConfig.builder()
+                    .withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.UPDATE_SKIP_NULL_ATTRIBUTES)
+                    .build());
   }
 }
